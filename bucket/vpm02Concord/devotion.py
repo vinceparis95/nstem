1592152c6323386eb7e759f9ef74096d3f06b9ee
Vincent Paris <vincentparis@vincents-mbp.myfiosgateway.com>
2020-05-04 12:20:37 -0400

    test commit

--- bucket/vpm02Concord/devotion.py
+++ bucket/vpm02Concord/devotion.py
@@ -4,16 +4,31 @@ import matplotlib.pyplot as plt
 
 
 devotions = pd.read_excel("~/nstemData/devoQ1.xlsx")
-devotions.rename(columns={'Host':'day'}, inplace=True)
-devotions.set_index('day')
-devotions = devotions.loc[1:64,'day':'12/29 F']
+
+devotions = devotions.loc[1:64,'Host':'4/26 F']
+devotions = devotions.rename(columns={'Host':'day'})
 devotions = devotions.drop(['Sub-cluster',
                             'Sector/Locality', 'Frequency',
                             'Count'], axis=1)
 devotions = devotions.fillna(0)
-# devotions = devotions.transpose()
-print(devotions.head())
+devotions = devotions.set_index('day').T
+
+
+print(devotions)
 print(list(devotions.columns))
 
+mohadjeri = devotions['Mohadjeri family']
+
+plot = devotions.plot(legend=None,linewidth=7, alpha=0.14)
+plot.set_ylabel('number')
+plot.set_xlabel('days')
+plt.xticks(fontsize=9, rotation=45)
+plt.locator_params(axis='x',nbins=19)
+
+plt.show()
+
+# plot2 = devotions.plot(kind= "scatter", x='Host', y='sum',
+# legend=None,linewidth=7, alpha=0.14)
+# plt.show()
 
 # devotions[['Mohadjeri family','Anisa new Bk 1 w/Nagiarry']].plot()
